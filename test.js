7701050cd3b21f043e55fb15e4b620aff1537914
Maja Kasprzyk <maja.kasprzyk@gmail.com>
2020-09-24 00:34:17 +0200

    12 Is statments

--- test.js
+++ test.js
@@ -1,2 +1,7 @@
-// the first alert
-alert("Hello World!");
+var youLikeMeat = false;
+if (youLikeMeat){
+    document.write("Here is the meaty menu");
+}
+else{
+    document.write("you hate meat")
+}
