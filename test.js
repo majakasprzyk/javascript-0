2bfe495864a02b287d7ff8d65db9f1235e5fe022
Maja Kasprzyk <maja.kasprzyk@gmail.com>
2020-09-24 22:27:27 +0200

    13 else if statements

--- test.js
+++ test.js
@@ -1,7 +1,14 @@
-var youLikeMeat = false;
-if (youLikeMeat){
-    document.write("Here is the meaty menu");
+var myAge = 9;
+
+if (myAge > 30){
+    document.write("you are over 30");
+} 
+else if(myAge > 20){
+    document.write("you are over 20");
 }
-else{
-    document.write("you hate meat")
+else if(myAge > 10){
+    document.write("you are over 10");
+}
+else {
+    document.write("you are not over 10");
 }
