f2f3eaed55ce0a469b6f8887ca8bcc8073ebd625
Maja Kasprzyk <maja.kasprzyk@gmail.com>
2020-09-25 00:20:31 +0200

    20 Function

--- test.js
+++ test.js
@@ -1,13 +1,10 @@
-for (i = 0; i < 10; i++) {
+function getAverage (a,b,c,d,e,f){
 
-    if(i === 5 || i === 3){
-        continue;
-    }
+    var average = (a + b + c + d + e +f) / 6;
+    console.log(average);
+    return average;
 
-    console.log(i);
-
-    if(i === 7){
-        break
-    }
 }
-console.log("break the loop")
+
+var myResult = getAverage(7,8,9,10,11,12);
+console.log("the average is " + myResult);
