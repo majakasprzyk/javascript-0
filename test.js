e96d48ed622c1b03d0acc747bb6adda2ab9d4b7a
Maja Kasprzyk <maja.kasprzyk@gmail.com>
2020-10-08 23:12:18 +0200

    30 - Constructor Functions

--- test.js
+++ test.js
@@ -5,11 +5,27 @@ var myCar2 = {
     drive: function(speed, time){
         console.log(speed * time);
     },
-    test: function(){
-        console.log(this);
+    logDriver: function(){
+        console.log("driver name is " + this.driver);
     }
 };
 
-myCar2.test();
-console.log(myCar2.maxSpeed);
-myCar2.drive(50, 3);
+var Car = function(maxSpeed, driver){
+
+    this.maxSpeed = maxSpeed;
+    this.driver = driver;
+    this.drive = function(speed,time){
+        console.log(speed * time);
+    };
+    this.logDriver = function(){
+        console.log("driver name is " + this.driver);
+    };
+}
+
+var myCar = new Car(70, "Maja");
+var myCar2 = new Car(50, "MAdam");
+var myCar3 = new Car(40, "Maciek");
+var myCar4 = new Car(30, "Ludi");
+
+myCar.drive(30,5);
+myCar3.logDriver();
