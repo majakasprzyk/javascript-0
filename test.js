e3b980e775d7c3e317de8ef4ad7305f91b77e4ec
Maja Kasprzyk <maja.kasprzyk@gmail.com>
2020-10-08 22:37:32 +0200

    28 - Creating a new JavaScript Object

--- test.js
+++ test.js
@@ -1,3 +1,21 @@
-var myString = new String();
-myString = "hello";
-var myString2 = "hi there";
+var myArray = new Array();
+myArray[0] = 8;
+myArray[1] = "hello";
+
+var myCar = new Object();
+myCar.maxSpeed = 50;
+myCar.driver = "Maja";
+myCar.drive = function() {console.log("now driving");};
+
+myCar.drive();
+
+var myCar2 = {
+    maxSpeed: 70,
+    driver: "Mjcksp",
+    drive: function(speed, time){
+        console.log(speed * time);
+    }
+};
+
+console.log(myCar2.maxSpeed);
+myCar2.drive(50, 3);
